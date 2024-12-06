f27cef01739dac0738ea634498f674bb9ba37915
Yared <alemayehuyared9@gmail.com>
2024-12-06 05:58:36 -0800

    User Autentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router=express.Router();
 const db=require('../db');
+
+//signup
 router.post('/signup',async (req,res)=>{
     const formData=req.body;
     const {
@@ -16,8 +18,20 @@ router.post('/signup',async (req,res)=>{
         PASSWORD:password
     }=formData
     try{
-    const hashedpassword= await bcrypt.hash(password,10)
-    db.query("INSERT INTO user_info (first_name,last_name,email,phone_number,country,region ,city,subcity,password) VALUES(?,?,?,?,?,?,?,?,?)",
+    const query1='SELECT *FROM user_info WHERE email=?';
+    db.query(query1,[email], async (err,results)=>{
+        if(err){
+            console.error('Database error:',err);
+            return res.status(500).json({error:'Database error'});
+        }
+        if(results.length>0){
+            return res.status(400).json({error:'Email already exist'});
+        }
+        const hashedpassword= await bcrypt.hash(password,10);
+        const query="INSERT INTO user_info (first_name,last_name,email,phone_number,country,region ,city,subcity,password) VALUES(?,?,?,?,?,?,?,?,?)";
+
+   
+    db.query(query,
         [fname,lname,email,phone_no,country,region,city,subcity,hashedpassword],
         (err,results) =>{
             if(err){
@@ -25,13 +39,37 @@ router.post('/signup',async (req,res)=>{
                 res.status(500).json({error:"something bad happens try again"});
                 return;
             }
-        
              res.json({ message:'From data received successfully!'});
 
+            });
         });}
      catch(error){
         res.status(500).json({error:"Internal server error"});
      }   
 
 });
+//login
+router.post('/login',(req,res)=>{
+    const {EMAIL,PASSWORD}=req.body
+    const query='SELECT * FROM user_info  WHERE email= ?'
+    db.query(query,[EMAIL],async (err,results)=>{
+        if(err) throw err;
+        if(results.length>0){
+            const user =results[0];
+            console.log(user.email);
+            console.log(PASSWORD);
+            const isMatch= await  bcrypt.compare(PASSWORD,user.password);
+            if(isMatch){
+                res.status(200).json({message:'login successfully'});
+                console.log('user exists');
+            }
+            else{
+                res.status(401).json({message:'Invalid credential'});
+                console.log('user exists inv');
+            }
+        }
+        else 
+            res.status(404).json({message:'User not found'});
+    })
+})
 module.exports=router;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,30 +1,61 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useNavigation } from "react-router-dom";
 import SignupForm from "./signup";
+import axios from "axios";
 const LoginForm = ()=>{
-    const navigate=useNavigate()
-    function handleSubmit(event){
-        event.preventDefault();
-        const form=event.target;
-        if(form.checkValidity()){
-            navigate('/Identifier')
-        }
-        else{
-            form.reportValidity()
-        }
+    const [formData,setFormData]=useState({
+        EMAIL:'',
+        PASSWORD:''
+    })
+    const [error,setError]=useState('')
+    const navigate=useNavigate();
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setFormData({ ...formData,[name]:value});
 
+    }
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const form=e.target;
+        if(form.checkValidity()){
+          try{
+            const response=await axios.post('http://localhost:5000/auth/login',formData);
+            if(response.status==200){
+              navigate('/Identifier');
+
+            }
+            else{
+                console.log("cant access ")
+            }
+          }
+          catch(error){
+            if(error.response){
+                setError(error.response.data);
+                      }
+            else{
+                setError('An error occurred. please try again later.')
+                console.log('Error sending form data:',error);
+            }
+
+        }
+        
+
+    }
+         else{
+             form.reportValidity();
+         }
     }
     return(
       <div className="flex flex-col items-center">
         <form onSubmit={handleSubmit} className=" mt-32 p-6 rounded bg-white shadow-lg">
             <div>
                 <label className="block font-medium mb-4">EMAIL</label>
-                <input className="w-full p-2 border border-gray-300" required/>
+                <input type ='email' className="w-full p-2 border border-gray-300" name="EMAIL" value={formData.EMAIL} onChange={handleChange} required/>
             </div>
             <div>
                 <label className="block mb-4 font-medium ">PASSWORD</label>
-                <input className="border border-gray-300 w-full p-2" required/>
+                <input type="password" className="border border-gray-300 w-full p-2" name="PASSWORD" value={formData.PASSWORD} onChange={handleChange} required/>
             </div>
             <div>
              <button type="submit" className="border mt-8 mx-[150px] bg-blue-500 hover:bg-blue-300 p-2 rounded w-4xl text-white">Submit</button>
