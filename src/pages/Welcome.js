29be537c7991fc1148f3f39c929fb7fcd91b86a7
Yared <alemayehuyared@gmail.com>
2024-12-08 04:10:54 -0800

    UX Modification

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,10 +5,10 @@ function Welcome(){
         <h1 className='text-center mt-[210px] mb-10 text-6xl animation-marquee  font-bold'> Welcome To The DH </h1>
         <div className='space-x-4 text-center'>
             <Link to="/SignupForm">
-            <button className='bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>Sign Up </button>
+            <button className='bg-blue-900 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>Sign Up </button>
             </Link>
             <Link to="/LoginForm">
-            <button className='bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>Login</button>
+            <button className='bg-blue-900 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>Login</button>
             </Link>
         </div>
     </div>);
