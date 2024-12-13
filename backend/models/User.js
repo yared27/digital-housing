0fe4cf6ca1fc534fd09fa1687217e72f1db82a96
Yared <alemayehuyared9@gmail.com>
2024-12-13 02:27:22 -0800

    Adding auto password generator from external API

--- backend/models/User.js
+++ backend/models/User.js
@@ -48,17 +48,3 @@ const user=db.define('user',{
 });
 module.exports=user
 
-// const mongoose =require('mongoose');
-// const userSchema= new mongoose.Schema({
-//     firstName:String,
-//     lastName:String,
-//     email:{type:String,required :true,unique :true},
-//     phone_number:Number,
-//     country:String,
-//     region:String,
-//     city:String,
-//     subcity:String,
-//     password:{type:String,required:true},
-
-// })
-// module.exports=mongoose.model('user',userSchema);
