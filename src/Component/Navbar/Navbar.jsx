a24b8168641734caa092c4bc9bbcd97f4cae2db5
Yared <alemayehuyared9@gmail.com>
2024-12-30 00:47:12 -0800

    Navbar Maintenance

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 const Navbar = () =>{
+    const [IsOpen,setIsOpen]=useState(false);
+    const handleMouserEnter=()=>setIsOpen(true);
+    const handleMouseleave=()=>setIsOpen(false)
     return (
-<nav className="bg-blue-900 p-4 fixed w-full z-20 top-0 start-0 border-b border-gray-200  ">
-    <div className=" mx-w-screen-xl flex-wrap items-center justify-between mx-auto  p-4 ">
-        <ul className="flex space-x-4">
-            <li><Link to={"/"} className="text-white hover:text-blue-300" >HOME</Link></li>
-            <li><Link to={"#"} className="text-white hover:text-blue-300" href="#">ABOUT</Link></li>
-            <li><Link to= "%" className="text-white hover:text-blue-300" >SERVICE</Link></li>
-            <li><Link to={"@"} className="text-white hover:text-blue-300">CONTACT</Link></li>
-        </ul>
+<nav onMouseLeave={handleMouseleave} className="bg-white dark:bg-blue-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  </a>
+  <div onMouseEnter={handleMouserEnter}  className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <Link to={"/SignupForm"}  type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</Link>
+      <button   type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
+        <span class="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div className={`items-center justify-between  w-full md:flex md:w-auto md:order-1 ${IsOpen? 'block':'hidden'}`} id="navbar-sticky">
+    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-500 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-blue-900 dark:bg-gray-800 md:dark:bg-blue-900 dark:border-gray-700">
+            <li><Link to={"/"} className="block py-2 px-3 text-gra-900 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-100" aria-current="page" >HOME</Link></li>
+            <li><Link to={"#"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" href="#">ABOUT</Link></li>
+            <li><Link to= "%" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >SERVICE</Link></li> 
+            <li><Link to={"@"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">CONTACT</Link></li>
+            </ul>
+        </div>
     </div>
 </nav>
     );
