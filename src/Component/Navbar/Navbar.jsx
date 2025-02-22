5a119d8cfbb7818a4271f848c6a18ddc4d490001
Yared <alemayehuyared9@gmail.com>
2025-02-22 13:09:52 -0800

    Fix homepage layout and styling

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar_page = () =>{
   </Link>
   <div   className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
     {location.pathname==='/'  && (
-      <Link to={"/SignupForm"}  type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</Link>
+      <Link to={"/SignupForm"}  type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mx-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</Link>
     )}
       <DropdownMenu/>
       <button onMouseEnter={handleMouserEnter}  type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 function Welcome(){
    return( <div>
-        <h1 className='text-center mt-[210px] mb-10 text-4xl animation-marquee   font-bold'> Welcome To The DH </h1>
-        <div className="space-x-4 text-center " >
-            <Link to="/SignupForm">
+        <div className="space-x-4  text-center h-screen bg-cover " style={{backgroundImage:"url('/images/im.jpg')"}} >  
+
+        </div>
+        {/* text-center mt-[210px] mb-10 text-4xl */}
+        <div className='border-t  mt-[200 items-center px] flex flex-col border-gray-300  bg-blue-900 h-screen'>
+           <h1 className=' animation-marquee text-white text-[40px] mb-2 mt-[100px] font-bold'> Welcome To The DH </h1>
+           <div className='flex space-x-4 '>
+           <Link to="/SignupForm">
             <button className='bg-blue-900 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>Sign Up </button>
             </Link>
             <Link to="/LoginForm">
-            <button className='bg-blue-900 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>Login</button>
+            <button className='bg-blue-900 hover:bg-blue-500 text-white  font-bold py-2 px-4 rounded'>Login</button>
             </Link>
-            
-        </div>
-        <div className='border-t mt-[200px] border-gray-300 my-8 bg-blue-900 h-screen'>
+            </div>
         </div>
     </div>);
 }
