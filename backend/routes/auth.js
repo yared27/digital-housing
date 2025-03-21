1d2a7112ae2dc08c7653873cb4a7c9616fef94dd
Yared <alemayehuyared9@gmail.com>
2025-03-21 11:51:14 -0700

    fix: replaced bcrypt with bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import {user, properties } from '../models/User.js';
 // routes/owner_property.js
