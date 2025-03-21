558f9cd7998e970493de30cc4ce9f6de67ed9840
Yared <alemayehuyared9@gmail.com>
2025-03-21 12:26:18 -0700

    process.env typo correction

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 // require('dotenv').config();
 const sequelize =new Sequelize(process.env.DB_NAME,
-    process.env.DB_NAME,
+    process.env.DB_USER,
     process.env.DB_PASSWORD,
     {host:"localhost",
      port:3306,
