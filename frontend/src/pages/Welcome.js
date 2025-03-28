c1718071d62dc67157c243f6973e8d80697c9445
Yared <alemayehuyared9@gmail.com>
2025-03-28 01:08:16 -0700

    make responsive for xl screen

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 function Welcome(){
    return( <div>
-        <div className="space-x-4 w-full max-w-3xl h-screen bg-cover flex justify-left  items-center sm:text-center  " style={{backgroundImage:"url('/images/im.jpg')"}} > 
+        <div className="space-x-4 w-full max-w-8xl h-screen bg-cover flex justify-left  items-center sm:text-center  " style={{backgroundImage:"url('/images/im.jpg')"}} > 
          <div>
           <h1 className='mx-[20px] text-left mb-40 animation-marquee text-white text-[40px]  font-bold'> Welcome To The DH </h1>
           </div>
         </div>
         {/* text-center mt-[210px] mb-10 text-4xl */}
-        <div className='border-t w-full max-w-3xl  items-center px-4 sm:px-6 md:px-8 flex flex-col border-gray-300  bg-blue-600 h-screen'>
+        <div className='border-t w-full max-w-8xl  items-center px-4 sm:px-6 md:px-8 flex flex-col border-gray-300  bg-blue-600 h-screen'>
             <div className='text-center w-full max-w-3xl sm:text-left w-full max-w-4xl '>
               <h1 className=' text-center sm:text-left  mb-[50px] w-[800px] text-[20px] text-white mt-12 sm:mt-20 mb-8 sm:mb-12 mx-auto px-4 max-w-md md:max-w-lg lg:max-w-xl  text-base sm:text-lg md:text-xl leading-normal `'>  At Digital Housing, we revolutionize the way you find and manage homes.
                 Our platform offers a seamless experience for browsing rental properties,
