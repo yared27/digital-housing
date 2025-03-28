a63caf1375528d501b9fd22a26434141fbb06d6e
Yared <alemayehuyared9@gmail.com>
2025-03-27 22:47:18 -0700

    Make the welcome page responsive

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import './index.css';
 import App from './App';
+import 'react-toastify/dist/ReactToastify.css'
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
