4c9161935022d01e3b9053f3d095042521f64e66
Yared <alemayehuyared9@gmail.com>
2025-03-29 13:40:46 -0700

    justifying the paragraph for mobile view

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -8,16 +8,16 @@ function Welcome(){
           </div>
         </div>
         {/* text-center mt-[210px] mb-10 text-4xl */}
-        <div className='border-t w-full   md:w-lg  px-4 sm:item-left md:px-8 flex flex-col border-gray-300  bg-blue-600 min-h-screen'>
+        <div className='border-t w-full items-center  md:w-lg  px-4 sm:item-left md:px-8 flex flex-col border-gray-300  bg-blue-600 min-h-screen'>
             <div className='text-center flex flex-col  sm:text-left w-full max-w-4xl '>
-              <p className='	max-w-prose center sm:text-left break-words mb-[50px] w-[800px]  text-white mt-12 sm:mt-20  sm:mb-12 mx-auto px-4  md:max-w-lg lg:max-w-3xl  text-base sm:text-lg md:text-xl leading-normal '>  At Digital Housing, we revolutionize the way you find and manage homes.
+              <p className='	max-w-prose  text-left sm:justify   mb-8  text-white mt-12 sm:mt-20  sm:mb-12 mx-auto px-4  md:max-w-lg lg:max-w-3xl  leading-relaxed  text-base sm:text-lg md:text-xl  '>  At Digital Housing, we revolutionize the way you find and manage homes.
                 Our platform offers a seamless experience for browsing rental properties,
                 exploring virtual tours, and managing your housing needs online. 
                 Whether you're a renter looking for the perfect space or a landlord wanting to showcase your property, 
                 we've got you covered with modern technology and user-friendly features.
                 Start your journey with us today and discover a smarter, easier way to manage your housing experience!</p>
             </div>
-           <div className='flex px-12 space-x-2 '>
+           <div className='flex  space-x-2 '>
            <Link to="/SignupForm">
             <button className='bg-blue-900 bg-blue-500 mb-2 hover:bg-blue-700 text-white font-bold mx-[20px] py-2 px-4 rounded'>Sign Up </button>
             </Link>
